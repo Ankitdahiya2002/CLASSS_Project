b64dd5b02e6307d9e6ded252bef2b42c484f6384
Ankitdahiya2002 <103168119+Ankitdahiya2002@users.noreply.github.com>
2025-10-03 16:47:28 +0530

    Update helper.py
    gemini model update gemini-1.0

--- src/helper.py
+++ src/helper.py
@@ -20,7 +20,7 @@ def gemini_model_object(user_input):
     if not genai:
         return "Gemini is not properly configured. Check API key or SDK."
     try:
-        model = genai.GenerativeModel("models/gemini-1.5-flash-latest")
+        model = genai.GenerativeModel("models/gemini-1.0")
 
         response = model.generate_content({
             "parts": [
